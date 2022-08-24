703d226c475c200a08f06365a62da19992825cad
BhagyashreeNemade <108821937+BhagyashreeNemade@users.noreply.github.com>
2022-08-24 22:06:23 +0530

    add contact in address book

--- Program.cs
+++ Program.cs
@@ -1,18 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace AddressBook
 {
-    class Contact
+    class Program
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string address { get; set; }
-        public string city { get; set; }
-        public string state { get; set; }
-        public int zip { get; set; }
-        public long PhoneNumber { get; set; }
-        public string email { get; set; }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Address Book Program on Master Branch ");
+            Console.WriteLine("\n");
+            Console.WriteLine("Add the Contact Details ");
+            Console.WriteLine("\n");
+            AddressBookSystem addressbook = new AddressBookSystem();
+            Contact contact = new Contact();
+            SetDetails(contact);
+            addressbook.AddContact(contact);
+        }
+        public static void SetDetails(Contact contact)
+        {
+            Console.WriteLine("First Name : ");
+            contact.FirstName = Console.ReadLine();
+            Console.WriteLine("Last Name :");
+            contact.LastName = Console.ReadLine();
+            Console.WriteLine("Address :");
+            contact.Address = Console.ReadLine();
+            Console.WriteLine("City :");
+            contact.City = Console.ReadLine();
+            Console.WriteLine("State :");
+            contact.State = Console.ReadLine();
+            Console.WriteLine("Zip Code :");
+            contact.Zip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Phone Number :");
+            contact.PhoneNumber = long.Parse(Console.ReadLine());
+            Console.WriteLine("Email Address :");
+            contact.Email = Console.ReadLine();
+        }
     }
+
 }
